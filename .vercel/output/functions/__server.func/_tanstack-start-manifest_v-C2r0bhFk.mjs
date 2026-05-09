2cccc358b7fdb41b429b0ac0a75b1a0af86b2a9d
dinulkithsara <kithsaradinul@gmail.com>
2026-05-09 13:09:51 +0530

    chore: ignore .vercel directory

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-C2r0bhFk.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/dinulkithsara/dinul-kithsara/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-Cz7wz-r8.js"] }, "/": { filePath: "/Users/dinulkithsara/dinul-kithsara/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-q9MPnxuI.js"] } }, clientEntry: "/assets/index-Cz7wz-r8.js" });
-export {
-  tsrStartManifest
-};
